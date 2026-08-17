473a89c4f6510b03a636696a3c6f6def3a290577
Glyden <glydenvr7@gmail.com>
2026-08-17 17:27:59 -0300

    produtos

---
+++ js/products.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+        id: 1,
+        name: "Fone Ilidan",
+        category: "tecnologia",
+        price: 170,
+        description: "Fone sem fio com estojo de conexão Bluetooth."
+    },
+
+    {
+        id: 2,
+        name: ,
+        category: ,
+        price: 
+        description: "."
+    }
+    {
+        id: 3,
+        name: ,
+        category: ,
+        price: 
+        description: "."
+    }
+    {
+        id: 4,
+        name: ,
+        category: ,
+        price: 
+        description: "."
+    }
+    {
+        id: 5,
+        name: ,
+        category: ,
+        price: 
+        description: "."
+    }
+]
